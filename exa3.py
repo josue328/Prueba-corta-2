27dd8741e92856c59871ef12e828cd41d95bcbbb
Yindra9 <101944223+Yindra9@users.noreply.github.com>
2022-03-19 19:56:45 -0600

    Add files via upload

---
+++ exa3.py
@@ -0,0 +1,45 @@
+num=77635245
+print ("Numero es",num)
+divi=10
+suma=0
+m=0
+
+n=num%divi
+num=num//divi
+p=num%divi
+num=num//divi
+t=num%divi
+num=num//divi
+o=num%divi
+num=num//divi
+u=num%divi
+num=num//divi
+v=num%divi
+num=num//divi
+e=num%divi
+num=num//divi
+r=num%divi
+num=num//divi
+re=n+p+t+o+u+v+e+r
+
+print("suma de los digitos del numero",re)
+cla=re%divi
+s1=cla+n
+s2=cla+p
+s3=cla+t
+s4=cla+o
+s5=cla+u
+s6=cla+v
+s7=cla+e
+s8=cla+r
+des1=s1%divi
+des2=s2%divi
+des3=s3%divi
+des4=s4%divi
+des5=s5%divi
+des6=s6%divi
+des7=s7%divi
+des8=s8%divi
+print ("Numero Descifrado",0,".",des8,des7,des6,des5,des4,des3,des2,des1)
+
+          
